1a8a614129a1453a696287067f9ad2f76a0fed27
Alex Zorzi <info@alexzorzi.it>
2020-10-14 16:10:00 +0200

    moved sig_handler
    Co-authored-by: Catta1997 <34333654+Catta1997@users.noreply.github.com>

--- start.py
+++ start.py
@@ -1,6 +1,9 @@
 import signal
 import sys
 from animesaturn import AnimeSaturn
+def sig_handler(_signo, _stack_frame):
+    print("\n")
+    sys.exit(0)
 def main():
     signal.signal(signal.SIGTERM, sig_handler)
     signal.signal(signal.SIGINT, sig_handler)
