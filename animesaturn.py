c00c1efb4f28784d1e2d987d2ada6151360173ce
Catta1997 <edoardo.cattarin@gmail.com>
2020-10-18 11:19:58 +0200

    limit of downloads
    - nel config.ini ora puoi scegliere quanti download eseguire contemporaneamente
    - fix generali

--- animesaturn.py
+++ animesaturn.py
@@ -1,24 +1,23 @@
-import requests
-import os
-import sys
-import signal
-import psutil
-from bs4 import BeautifulSoup
-import re
-from datetime import datetime
-import locale
-import time
-from tqdm import tqdm
 import concurrent.futures
 import configparser
+import locale
+import os
+import psutil
+import re
+import requests
+import signal
+import sys
+import time
+
+from bs4 import BeautifulSoup
+from datetime import datetime
+from tqdm import tqdm
 
 class AnimeSaturn:
     #config stuff
     dir_path = os.path.dirname(os.path.realpath(__file__)) + '/'
-
-    debug = True
+    verbose = False
     titolo = ""
-
     only_link = list()
     list_link = list()
     correlati_list = list()
@@ -33,7 +32,6 @@ class AnimeSaturn:
             self.file_type = 0
         else:
             self.test_ID = False
-
         self.config = configparser.ConfigParser()
         self.config.read('config.ini')
         self.import_config()
@@ -55,7 +53,8 @@ class AnimeSaturn:
             print("\x1b[31mValore inserito nel config.ini non valido\x1b[0m")
             print("Inserire manualmente la tipologia di programma: ")
             self.seleziona()
-        print(self.test_ID)
+        if(self.verbose):
+            print(self.test_ID)
         if(self.test_ID):
             self.search("love is war")
         else:
@@ -74,16 +73,21 @@ class AnimeSaturn:
                     print("\x1b[31mScelta non valida, riprovare...\x1b[0m")
 
     def import_config(self):
+        if (self.config["DEFAULT"].getint("limit") is (None or "")):
+            self.limit = -1
+        else:
+            try:
+                self.limit = int(self.config["DEFAULT"].getint("limit"))
+            except ValueError:
+                self.limit = -1
         if (self.config["DEFAULT"].get("crawl_path") is (None or "")):
             self.crawl_path = self.dir_path + "crawl_path/"
         else:
             self.crawl_path = self.config["DEFAULT"]['crawl_path']
-
         if (self.config["DEFAULT"].get("download_path") is (None or "")):
             self.download_path = self.dir_path + "download_path/"
         else:
             self.download_path = self.config["DEFAULT"]['download_path']
-
         if (self.config["DEFAULT"].get("movie_folder") is (None or "")):
             self.movie_path = self.dir_path + "movie_folder/"
         else:
@@ -102,7 +106,8 @@ class AnimeSaturn:
     def selected_anime(self, url):
         ep_list = list()
         mutex = False
-        print(url)
+        if(self.verbose):
+            print(url)
         #visito la pagina, trovo il tasto per l'episodio. Sucessivamente analizzo quella  pagina e ottengo il link di streaming
         new_r = requests.get(url = url, params = {})
         pastebin_url = new_r.text
@@ -129,7 +134,7 @@ class AnimeSaturn:
             ep_list.append(episode)
         mutex = False
         with concurrent.futures.ThreadPoolExecutor(max_workers=len(ep_list)) as pool:
-            if(self.debug):
+            if(self.verbose):
                 print(os.getpid())
                 for t in pool._threads:
                     print(t)
@@ -148,8 +153,6 @@ class AnimeSaturn:
         self.all_ep[episode] = x[1]
         self.list_link.append([episode,int(x[0].split("-")[-1])])
 
-
-
     def get_correlati(self, url):
         is_lang = "-ITA" in url #controlla se il link supporta la lingua ita
         #analizzo url e cerco la sezione "correlati" e richiamo la funzione per trovare gli episodi per gonuno di essi
@@ -167,17 +170,18 @@ class AnimeSaturn:
                 self.correlati_list.append(anime)
         self.reorder_correlati()
 
-    @staticmethod
-    def kill_child_processes(parent_pid, sig=signal.SIGTERM):
+    def kill_child_processes(self, parent_pid):
         try:
             parent = psutil.Process(parent_pid)
         except psutil.NoSuchProcess:
             return
         children = parent.children(recursive=True)
         for process in children:
-            process.send_signal(sig)
+            process.send_signal(signal.SIGTERM)
+        if(self.verbose):
+            print("Killed %d processes"%len(children))
+
     def reorder_correlati(self):
-        #global titolo
         for URL in self.correlati_list:
             new_r = requests.get(url = URL, params = {})
             pastebin_url = new_r.text
@@ -191,11 +195,10 @@ class AnimeSaturn:
         for x in ordered_data:
             self.only_link.append(x[1])
         with concurrent.futures.ThreadPoolExecutor(max_workers=len(self.only_link)) as pool:
-            if(self.debug):
+            if(self.verbose):
                 for t in pool._threads:
                     print(t)
             pool.map(self.selected_anime, self.only_link)
-
 
     def search(self,name):
         anime_list  = list()
@@ -205,20 +208,21 @@ class AnimeSaturn:
         html = pastebin_url
         parsed_html = BeautifulSoup(html,"html.parser")
         animes = parsed_html.find_all('ul', attrs={'class':'list-group'})
-        x = 1
+        id_num = 1
         if (len(animes)) == 0:
             print("\x1b[31mNessun Anime trovato per %s\x1b[0m"%name)
             exit(0)
-        for dim in animes:
             print("--------")
-            print(x)
+        for dim in animes:
+            print("\x1b[36mID:")
+            print(id_num + "\x1b[0m")
             print("TITOLO:")
             print("\x1b[32m" + dim.find('a',attrs={'class':'badge badge-archivio badge-light'}).text + "\x1b[0m")
             print("TRAMA:")
             print("\x1b[37m" + dim.find('p',attrs={'class':'trama-anime-archivio text-white rounded'}).text +"\x1b[0m")
             anime_list.append(dim.find('a')['href'])
             print("--------")
-            x+=1
+            id_num += 1
         if(self.test_ID):
             selected = 1
         else:
@@ -233,7 +237,6 @@ class AnimeSaturn:
                     break
                 except ValueError:
                     print("\x1b[31mNon è un ID valido, riprovare...\x1b[0m")
-        #selected = 2
         selected -=1 #la lista parte da 0
         URL = anime_list[selected]
         self.season = 1
@@ -244,9 +247,10 @@ class AnimeSaturn:
             self.downloader()
         else:
             sys.exit(0)
-    @staticmethod
-    def check_Path(crawl_path):
-        print(crawl_path)
+
+    def check_Path(self, crawl_path):
+        if(self.verbose):
+            print(crawl_path)
         if(not os.path.isdir(crawl_path)):
             os.makedirs(crawl_path)
 
@@ -258,7 +262,7 @@ class AnimeSaturn:
             with open("%s%s.crawljob"%(self.crawl_path,self.titolo), 'w') as f:
                 f.write(crwd)
                 f.close()
-            print("Creo crawljob per %d episodi"%len(self.list_link))
+            print("Creo crawljob per %d episodi..."%len(self.list_link))
             for link in self.list_link:
                 sourcehtml = requests.get(link[0]).text
                 source = re.findall("file: \"(.*)\",",sourcehtml)
@@ -280,12 +284,14 @@ class AnimeSaturn:
                 }
                 '''%(mp4_link,download)
             with open("%s%s.crawljob"%(self.crawl_path,self.titolo), 'a') as f:
-                print(self.crawl_path)
+                if(self.verbose):
+                    print(self.crawl_path)
                 f.write(crwd)
                 f.close()
             self.list_link.clear()
 
     def downloader(self):
+        download_link = list()
         #creo un file vuoto, se presente sovrascrivo
         self.check_Path(str(self.download_path)) #verifico che path esista
         print("Rilevati %d episodi"%len(self.list_link))
@@ -304,11 +310,9 @@ class AnimeSaturn:
                 print("Invalido!")
             except ValueError:
                 print("Invalido!")
-
         for episodedata in self.list_link:
             if(start <= episodedata[1] <= finish):
                 sourcehtml = requests.get(episodedata[0]).text
-
                 try:
                     mp4_link =  re.findall("file: \"(.*)\",",sourcehtml)
                 except IndexError:
@@ -316,16 +320,22 @@ class AnimeSaturn:
                 episodes.append(mp4_link)
         print("\n")
         for ep in sorted(episodes):
-            self.download(ep[0])
+            download_link.append(ep[0])
+        if(self.limit == -1):
+            limite = len(download_link)
+        else:
+            limite = self.limit
+        with concurrent.futures.ThreadPoolExecutor(max_workers=limite) as pool:
+            pool.map(self.download, download_link)
         self.list_link.clear()
     #riordino correlati e  selezionato in base alla data di uscita
 
 
     def download(self,url):
-        print(url)
+        if(self.verbose):
+            print(url)
         file_name = url.split("/")[-1]
         self.check_Path(os.path.join(self.download_path,file_name.split("_")[0]))
-
         with open(os.path.join(self.download_path,file_name.split("_")[0],file_name), "wb"):
             response = requests.get(url, stream=True)
             with tqdm.wrapattr(open(os.path.join(self.download_path,file_name.split("_")[0],file_name), "wb"), "write", desc=url.split('/')[-1], total=int(response.headers.get('content-length', 0))) as fout:
--- start.py
+++ start.py
@@ -1,8 +1,12 @@
+import os
 import signal
 import sys
+
 from animesaturn import AnimeSaturn
+
 def sig_handler(_signo, _stack_frame):
     print("\n")
+    AnimeSaturn.kill_child_processes(os.getpgid())
     sys.exit(0)
 def main():
     signal.signal(signal.SIGTERM, sig_handler)
@@ -10,7 +14,7 @@ def main():
     AnimeSaturn()
 
 def test(fixthetester):
-    AnimeSaturn(debug=True)
+    AnimeSaturn(debug = True)
     return 0
 
 if __name__ == "__main__":
